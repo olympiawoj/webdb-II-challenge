d7746c9564a75e410ac8d5028c2aa80919daf095
olympia <owojcik7@gmail.com>
2019-03-26 21:22:33 -0400

    Bug in post request

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -38,21 +38,21 @@ router.get("/:id", (req, res) => {
 
 //POST /api/zoos
 
-// router.post("/", (req, res) => {
-//   db("zoos")
-//     .insert(req.body)
-//     .then(zoos => {
-//       const zoo = zoos[0];
-//       db("zoos")
-//         .where({ id })
-//         // .first()
-//         .then(zoo => {
-//           res.status(201).json(zoo);
-//         });
-//     })
-//     .catch(error => {
-//       res.status(500).json(error);
-//     });
-// });
+router.post("/", (req, res) => {
+  db("zoos")
+    .insert(req.body)
+    .then(zoos => {
+      const zoo = zoos[0];
+      db("zoos")
+        .where({ id })
+        // .first()
+        .then(zoo => {
+          res.status(201).json(zoo);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 module.exports = router;
